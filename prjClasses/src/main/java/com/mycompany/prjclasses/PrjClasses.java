2fa86b44ab24393f6497ff6d89806fe3fde23f10
st10454003 <164016086+st10454003@users.noreply.github.com>
2025-03-08 14:19:03 +0200

    Updated code
    added the new code of creating objects, for classes

--- prjClasses/src/main/java/com/mycompany/prjclasses/PrjClasses.java
+++ prjClasses/src/main/java/com/mycompany/prjclasses/PrjClasses.java
@@ -11,6 +11,12 @@ package com.mycompany.prjclasses;
 public class PrjClasses {
 
     public static void main(String[] args) {
-        System.out.println("Hello World! I just did the github link up");
+        
+        newClass nc = new newClass(); // creating an object of newClass
+        
+        nc.helloClass(); // have to use variables name nc to communicate with the class 
+        
+        System.out.println(nc.strSeconClassVar); // output varibles from 2nd class.
+        
     }
 }
---
+++ prjClasses/src/main/java/com/mycompany/prjclasses/newClass.java
@@ -0,0 +1,21 @@
+package com.mycompany.prjclasses;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author lab_services_student
+ */
+public class newClass {
+    
+    public String strSeconClassVar = "Testing";
+    
+    public void helloClass()
+    {
+        System.out.println("Hello from the second class");
+    }
+    
+}
